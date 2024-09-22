0485d8c7a48d0861ffb70e82e6a6912d3164d5a5
ThiagoPuyelli <thiagopuyelli@gmail.com>
2024-09-22 13:28:56 -0300

    Añadir entrada generica

---
+++ src/main/java/utils/EntradaGenerica.java
@@ -0,0 +1,34 @@
+package utils;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Scanner;
+
+public class EntradaGenerica<T> {
+    private T entidad;
+    public EntradaGenerica (T entidad) {
+        this.entidad = entidad;
+    }
+
+    public void pedirDatos () {
+        for (Field field : entidad.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            //if (camposIgnorar != null && camposIgnorar.contains(field.getName())) {
+            //    continue;
+            //}
+            Scanner scanner = new Scanner(System.in);
+            Type type = field.getGenericType();
+            System.out.println(type.getTypeName());
+            Object dato = Input.getInput(scanner, type, field.getName());
+            try {
+                field.set(entidad, dato);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            field.setAccessible(false);
+        }
+    }
+
+}
