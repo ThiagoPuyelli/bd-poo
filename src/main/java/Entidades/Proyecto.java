a39df8147f796f3b1868a3c028787a99f06bd010
Mateo <mateodanielponti@outlook.com>
2024-09-27 15:18:56 -0300

    Entidades Proyecto y Trabaja

--- src/main/java/Entidades/Proyecto.java
+++ src/main/java/Entidades/Proyecto.java
@@ -1,4 +1,67 @@
 package Entidades;
 
+import javax.persistence.*;
+
+@Entity
 public class Proyecto {
+    @Id
+    @Column(nullable = false)
+    private int id_proyecto;
+    @Column(nullable = false, length = 20)
+    private String nombre;
+    @Column(nullable = false)
+    private double presupuesto;
+
+    @ManyToOne
+    @JoinColumn(name = "id_gerencia", foreignKey = @ForeignKey(name = "id_g"))
+    private int id_gerencia;
+
+
+
+    public int getId_gerencia() {
+        return id_gerencia;
+    }
+
+    public void setId_gerencia(int id_gerencia) {
+        this.id_gerencia = id_gerencia;
+    }
+
+    public void setPresupuesto(double presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+
+    public int getId_proyecto() {
+        return id_proyecto;
+    }
+
+    public void setId_proyecto(int id_proyecto) {
+        this.id_proyecto = id_proyecto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(float presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Proyecto{" +
+                "id_proyecto=" + id_proyecto +'\''+
+                ", nombre='" + nombre + '\'' +
+                ", presupuesto=" + presupuesto +
+                '}';
+    }
 }
--- src/main/java/Entidades/Trabaja.java
+++ src/main/java/Entidades/Trabaja.java
@@ -1,4 +1,53 @@
 package Entidades;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table( name = "TRABAJA")
 public class Trabaja {
+    @ManyToMany
+    @JoinColumn(name="dni", foreignKey = @ForeignKey(name="dni"))
+    @Column(nullable = false)
+    private String dni;
+    @ManyToMany
+    @JoinColumn(name = "id_proyecto", foreignKey = @ForeignKey(name = "id_proyecto"))
+    private int id;
+
+    private double horasSemanales;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public double getHorasSemanales() {
+        return horasSemanales;
+    }
+
+    public void setHorasSemanales(double horasSemanales) {
+        this.horasSemanales = horasSemanales;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Trabaja{" +'\''+
+        "dni='" + dni + '\'' +
+                ", id=" + id +
+                ", horasSemanales=" + horasSemanales +
+                '}';
+    }
 }
